5e12007da32173d1459200937b758900c4f94cf9
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-02-17 12:34:46 +0300

    Connect React to Redux

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {legacy_createStore as createStore, bindActionCreators} from 'redux'
-
+import {legacy_createStore as createStore} from 'redux'
 import reducer from './reducer';
-import * as actions from './actions';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
 
 const store = createStore(reducer);
-
-const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec)
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10)
-  rnd(value);
-})
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <></>
+      {/* <Counter
+        counter={getState().value}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+          const value = Math.floor(Math.random() * 10)
+          rnd(value);
+        }}
+      /> */}
+      <Provider store={store}>
+        <App/>
+      </Provider>
   </React.StrictMode>
 );
