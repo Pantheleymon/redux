7ea055f28f41e8fe5872d563c1cccad66136d40e
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-02-17 14:37:20 +0300

    Hero_admin_panel

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {legacy_createStore as createStore} from 'redux'
-import reducer from './reducer';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from './components/App';
+import App from './components/app/App';
+import store from './store';
 
-const store = createStore(reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import './styles/index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-        <App/>
-      </Provider>
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
