ef76042bdb7e799f0d402198c2278692cf18e39f
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-02-18 13:47:34 +0300

    refactor: async Thunk

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,8 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { activeFilterChanged } from '../heroesFilters/filtersSlice';
-import { fetchFilters } from '../../actions';
+import { activeFilterChanged, fetchFilters } from '../heroesFilters/filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 
